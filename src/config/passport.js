c3bf7f60beb1d2f86db81d23b5424be7403ec639
KolarDev <muhyideenabdulbasit11@gmail.com>
2025-01-27 15:37:59 +0100

    hi

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,44 +1,44 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
-const { generateToken } = require("./../controllers/auth");
+// const passport = require('passport');
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// const jwt = require('jsonwebtoken');
+// const dotenv = require('dotenv');
+// const { generateToken } = require("./../controllers/auth");
 
-dotenv.config();
+// dotenv.config();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback',
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      try {
-        // Here you would typically check your database for an existing user
-        const user = {
-          id: profile.id,
-          name: profile.displayName,
-          email: profile.emails[0].value,
-          photo: profile.photos[0].value,
-        };
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//       callbackURL: '/auth/google/callback',
+//     },
+//     async (accessToken, refreshToken, profile, done) => {
+//       try {
+//         // Here you would typically check your database for an existing user
+//         const user = {
+//           id: profile.id,
+//           name: profile.displayName,
+//           email: profile.emails[0].value,
+//           photo: profile.photos[0].value,
+//         };
 
-        // Generate JWT token
-        const token = generateToken(user.id);
+//         // Generate JWT token
+//         const token = generateToken(user.id);
 
-        return done(null, { user, token });
-      } catch (error) {
-        return done(error, null);
-      }
-    }
-  )
-);
+//         return done(null, { user, token });
+//       } catch (error) {
+//         return done(error, null);
+//       }
+//     }
+//   )
+// );
 
-// Serialize user to session
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
+// // Serialize user to session
+// passport.serializeUser((user, done) => {
+//   done(null, user);
+// });
 
-passport.deserializeUser((obj, done) => {
-  done(null, obj);
-});
+// passport.deserializeUser((obj, done) => {
+//   done(null, obj);
+// });
