c8f79c9cf8aff0190d1c227cdd50cfb6e6fd7cec
KolarDev <muhyideenabdulbasit11@gmail.com>
2025-01-27 11:59:43 +0100

    done

--- src/utils/appError.js
+++ src/utils/appError.js
@@ -1,17 +1,13 @@
-// Error handling midddleware
 class AppError extends Error {
-    constructor(message, statusCode) {
-      super(message);
-  
-      this.statusCode = statusCode;
-      // The error status is "failed" if its user's fault. Else it's "Error"
-      this.status = `${this.statusCode}`.startsWith("4") ? "Failed!" : "Error!";
-      this.message = message;
-      this.isOperational = true;
-      // Also capture error stack
-      Error.captureStackTrace(this, this.constructor);
-    }
+  constructor(message, statusCode) {
+    super(message);
+
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
   }
-  
-  module.exports = AppError;
-  
+}
+
+module.exports = AppError;
