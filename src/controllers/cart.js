acf8c4cacc667db6a31ac776f66f3a3c12190a6f
Salami Azeez <salamitopeazeez@gmail.com>
2025-02-06 05:39:49 +0100

    Updated addToCart

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -6,52 +6,66 @@ const AppError = require("./../utils/appError");
 
 
 // Users add event to cart
-const addToCart = catchAsync( async (req, res) => {
+const addToCart = catchAsync(async (req, res) => {
   const { eventId } = req.params;
- 
-    const cart = await Cart.findOne({ user: req.user.id });
+  const userId = req.user.id;
 
-    if (cart) {
+  let cart = await Cart.findOne({ user: userId });
+
+  if (!cart) {
+    // ADD USER AND EVENT TO CART
+    await Cart.create({ user: req.user.id, events: [eventId] });
+  } else {
+    // CHECK IF EVENT IS ALREADY IN CART
+    if (!cart.events.includes(eventId)) {
       cart.events.push(eventId);
+      await cart.save();
     } else {
-      await Cart.create({ user: req.user.id, events: [eventId] });
+      return res.status(200).json({ 
+        status: "success", 
+        message: "Event is already in the cart" 
+      });
     }
+  }
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        cart,
-      },
-    });
- 
+  cart = await Cart.findOne({ user: userId });
+
+  
+  res.status(200).json({
+    status: "success",
+    data: {
+      cart,
+    },
+  });
+
 });
 
-const removeFromCart = catchAsync( async (req, res) => {
- 
-    const { eventId } = req.params;
+const removeFromCart = catchAsync(async (req, res) => {
 
-    const cart = await Cart.findOne({ user: req.user.id });
+  const { eventId } = req.params;
 
-    if (cart) {
-      // Find the array index of the eventId
-      const eventIndex = cart.events.findIndex((item) => item === eventId);
-      // if the event is present, remove it from the array of events in the cart
-      if (eventIndex === -1) {
-        cart.events.splice(eventIndex, 1);
-      } else {
-        return new AppError("event is not in your cart!", 401);
-      }
+  const cart = await Cart.findOne({ user: req.user.id });
+
+  if (cart) {
+    // Find the array index of the eventId
+    const eventIndex = cart.events.findIndex((item) => item === eventId);
+    // if the event is present, remove it from the array of events in the cart
+    if (eventIndex === -1) {
+      cart.events.splice(eventIndex, 1);
     } else {
-      return new AppError("You don't have event in your cart", 401);
+      return new AppError("event is not in your cart!", 401);
     }
+  } else {
+    return new AppError("You don't have event in your cart", 401);
+  }
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        cart,
-      },
-    });
-  
+  res.status(200).json({
+    status: "success",
+    data: {
+      cart,
+    },
+  });
+
 });
 
 module.exports = {
