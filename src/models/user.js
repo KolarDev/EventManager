529bd51a492a0ec40f07d725986c05d8af319c9a
KolarDev <muhyideenabdulbasit11@gmail.com>
2025-05-09 01:39:59 +0100

    fix upload stream error

--- src/models/user.js
+++ src/models/user.js
@@ -40,6 +40,7 @@ const userSchema = new mongoose.Schema(
       },
       select: false,
     },
+    photo: String,
     role: {
       type: String,
       enum: ["user", "admin"],
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -9,6 +9,4 @@ cloudinary.config({
 });
 
 
-module.exports = {
-  cloudinary
-};
+module.exports = cloudinary;
